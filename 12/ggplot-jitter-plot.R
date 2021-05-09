fde490f1cb9e0e8a9e0b1ad6785e236eeca22cfe
Jeet1994 <pragyaditya.das94@gmail.com>
2021-05-09 11:55:48 +0530

    ggplot

---
+++ 12/ggplot-jitter-plot.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+data(mpg, package="ggplot2") # alternate source: "http://goo.gl/uEeRGu")
+theme_set(theme_bw())  # pre-set the bw theme.
+
+g <- ggplot(mpg, aes(cty, hwy))
+
+# Scatterplot
+g + geom_point() + 
+  geom_smooth(method="lm", se=F) +
+  labs(subtitle="mpg: city vs highway mileage", 
+       y="hwy", 
+       x="cty", 
+       title="Scatterplot with overlapping points", 
+       caption="Source: midwest")
