38090e6a38a469fd80aa8dc4d5795b705c53ebb9
Pragyaditya Das <pragyaditya.das94@gmail.com>
2021-05-12 21:48:46 +0530

    Basic NLP - Identifying deceitful reviews

---
+++ 15/basicNLP.R
@@ -0,0 +1,157 @@
+library(tidyverse) # general utility & workflow functions
+library(tidytext) # tidy implimentation of NLP methods
+library(topicmodels) # for LDA topic modelling 
+library(tm) # general text mining functions, making document term matrixes
+library(SnowballC) # for stemming
+library(readr)
+decrev <- read_csv("deceptive-opinion.csv")
+
+top_terms_by_topic_LDA <- function(input_text, # should be a columm from a dataframe
+                                   plot = T, # return a plot? TRUE by defult
+                                   number_of_topics = 4) # number of topics (4 by default)
+{    
+  # create a corpus (type of object expected by tm) and document term matrix
+  Corpus <- Corpus(VectorSource(input_text)) # make a corpus object
+  DTM <- DocumentTermMatrix(Corpus) # get the count of words/document
+  
+  # remove any empty rows in our document term matrix (if there are any 
+  # we'll get an error when we try to run our LDA)
+  unique_indexes <- unique(DTM$i) # get the index of each unique value
+  DTM <- DTM[unique_indexes,] # get a subset of only those indexes
+  
+  # preform LDA & get the words/topic in a tidy text format
+  lda <- LDA(DTM, k = number_of_topics, control = list(seed = 1234))
+  topics <- tidy(lda, matrix = "beta")
+  
+  # get the top ten terms for each topic
+  top_terms <- topics  %>% # take the topics data frame and..
+    group_by(topic) %>% # treat each topic as a different group
+    top_n(10, beta) %>% # get the top 10 most informative words
+    ungroup() %>% # ungroup
+    arrange(topic, -beta) # arrange words in descending informativeness
+  
+  # if the user asks for a plot (TRUE by default)
+  if(plot == T){
+    # plot the top ten terms for each topic in order
+    top_terms %>% # take the top terms
+      mutate(term = reorder(term, beta)) %>% # sort terms by beta value 
+      ggplot(aes(term, beta, fill = factor(topic))) + # plot beta by theme
+      geom_col(show.legend = FALSE) + # as a bar plot
+      facet_wrap(~ topic, scales = "free") + # which each topic in a seperate plot
+      labs(x = NULL, y = "Beta") + # no x label, change y label 
+      coord_flip() # turn bars sideways
+  }else{ 
+    # if the user does not request a plot
+    # return a list of sorted terms instead
+    return(top_terms)
+  }
+}
+top_terms_review<-top_terms_by_topic_LDA(decrev$text, number_of_topics = 2)
+plot(top_terms_review)
+
+# create a document term matrix to clean
+reviewsCorpus <- Corpus(VectorSource(decrev$text)) 
+reviewsDTM <- DocumentTermMatrix(reviewsCorpus)
+reviewsDTM_tidy <- tidy(reviewsDTM)
+custom_stop_words <- tibble(word = c("hotel", "room"))
+# remove stopwords
+
+reviewsDTM_tidy_cleaned <- reviewsDTM_tidy %>% # take our tidy dtm and...
+  anti_join(stop_words, by = c("term" = "word")) %>% # remove English stopwords and...
+  anti_join(custom_stop_words, by = c("term" = "word")) # remove my custom stopwords
+
+# reconstruct cleaned documents (so that each word shows up the correct number of times)
+cleaned_documents <- reviewsDTM_tidy_cleaned %>%
+  group_by(document) %>% 
+  mutate(terms = toString(rep(term, count))) %>%
+  select(document, terms) %>%
+  unique()
+head(cleaned_documents)
+new_top_terms_review<-top_terms_by_topic_LDA(cleaned_documents$terms, number_of_topics = 2)
+plot(new_top_terms_review)
+
+# stem the words (e.g. convert each word to its stem, where applicable)
+reviewsDTM_tidy_cleaned <- reviewsDTM_tidy_cleaned %>% 
+  mutate(stem = wordStem(term))
+
+# reconstruct our documents
+cleaned_documents <- reviewsDTM_tidy_cleaned %>%
+  group_by(document) %>% 
+  mutate(terms = toString(rep(stem, count))) %>%
+  select(document, terms) %>%
+  unique()
+
+# now let's look at the new most informative terms
+stem_top_terms_review<-top_terms_by_topic_LDA(cleaned_documents$terms, number_of_topics = 2)
+plot(stem_top_terms_review)
+# function that takes in a dataframe and the name of the columns
+# with the document texts and the topic labels. If plot is set to
+# false it will return the tf-idf output rather than a plot.
+top_terms_by_topic_tfidf <- function(text_df, text_column, group_column, plot = T){
+  # name for the column we're going to unnest_tokens_ to
+  # (you only need to worry about enquo stuff if you're
+  # writing a function using using tidyverse packages)
+  group_column <- enquo(group_column)
+  text_column <- enquo(text_column)
+  
+  # get the count of each word in each review
+  words <- text_df %>%
+    unnest_tokens(word, !!text_column) %>%
+    count(!!group_column, word) %>% 
+    ungroup()
+  
+  # get the number of words per text
+  total_words <- words %>% 
+    group_by(!!group_column) %>% 
+    summarize(total = sum(n))
+  
+  # combine the two dataframes we just made
+  words <- left_join(words, total_words)
+  
+  # get the tf_idf & order the words by degree of relevence
+  tf_idf <- words %>%
+    bind_tf_idf(word, !!group_column, n) %>%
+    select(-total) %>%
+    arrange(desc(tf_idf)) %>%
+    mutate(word = factor(word, levels = rev(unique(word))))
+  
+  if(plot == T){
+    # convert "group" into a quote of a name
+    # (this is due to funkiness with calling ggplot2
+    # in functions)
+    group_name <- quo_name(group_column)
+    
+    # plot the 10 most informative terms per topic
+    tf_idf %>% 
+      group_by(!!group_column) %>% 
+      top_n(10) %>% 
+      ungroup %>%
+      ggplot(aes(word, tf_idf, fill = as.factor(group_name))) +
+      geom_col(show.legend = FALSE) +
+      labs(x = NULL, y = "tf-idf") +
+      facet_wrap(reformulate(group_name), scales = "free") +
+      coord_flip()
+  }else{
+    # return the entire tf_idf dataframe
+    return(tf_idf)
+  }
+}
+# let's see what our most informative deceptive words are
+top_terms_by_topic_tfidf(text_df = decrev, # dataframe
+                         text_column = text, # column with text
+                         group_column = deceptive, # column with topic label
+                         plot = T) # return a plot
+
+
+# look for the most informative words for postive and negative reveiws
+top_terms_by_topic_tfidf(text_df = decrev, 
+                         text_column = text, 
+                         group = polarity, 
+                         plot = T)
+# get just the tf-idf output for the hotel topics
+reviews_tfidf_byHotel <- top_terms_by_topic_tfidf(text_df = reviews, 
+                                                  text_column = text, 
+                                                  group = hotel, 
+                                                  plot = F)
+
+
